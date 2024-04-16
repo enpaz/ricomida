6d386b6d408b6fe730e227e88eb7a3524bfbb451
Pedro <pedro.zuniga.n@gmail.com>
2024-04-15 22:44:07 -0400

    primera carga a git

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,35 @@
-// habilitar todos los tooltip de bootstrap
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+$(document).ready(function () {
+    // habilitar todos los tooltip de bootstrap
+    $('[data-bs-toggle="tooltip"]').tooltip()
+    // requerimiento 4: Utilizando los selectores de etiqueta 
+    // capturar el evento`dblclick` 
+    $("h3").dblclick(function () {
+    // modificar el color del texto de los títulos
+    // "INGREDIENTES" y "PREPARACIÓN" a color rojo
+        $(this).css({
+          "color": "red"
+        })
+    })
+
+    // requerimitneo 5: usar el método toggle desaparcer/aparecer
+    // el contenido en todas las tarjetas
+    $(".card-title").click(function () {
+	// esto debe suceder cuando el usuario haga un clic 
+	// sobre el título de cualquiera de las tres tarjetas.
+	$(this).next().toggle("slow")
+    
+})
+
+
+
+})
+
+// Evento: cuando se completo la transición de cambio de diapositiva...
+document.querySelector('#my-carousel').addEventListener('slide.bs.carousel', event => {
+    document.querySelectorAll(".indicador").forEach(element => {
+      element.classList.toggle("active");
+    })
+  })
+
+
+  
