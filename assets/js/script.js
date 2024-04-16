c57ac3b07ef71df24a0aaba760b2d3ea97f43925
Pedro <pedro.zuniga.n@gmail.com>
2024-04-15 20:04:03 -0400

    version 1

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// habilitar todos los tooltip de bootstrap
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
